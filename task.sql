d0ca31bfa4320758c0d928ba13e32f24eb43a7db
cthusq <cthusq.qusevi@gmail.com>
2024-03-29 12:35:41 +0200

    Solution

--- task.sql
+++ task.sql
@@ -4,10 +4,10 @@ INSERT INTO Orders (CustomerID, Date)
     VALUE (1, '2024-03-11');
 
 INSERT INTO OrderItems (OrderID, ProductID, Count)
-    VALUE (1, 1, 10);
+    VALUE (1, 1, 1);
 
 START TRANSACTION; 
-UPDATE Products SET WarehouseAmount = WarehouseAmount - 10 WHERE ID = '1';
+UPDATE Products SET WarehouseAmount = WarehouseAmount - 1 WHERE ID = '1';
 
 COMMIT;
 
